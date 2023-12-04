616b2bfa19ca59fe10d0669ea343b197f43dbded
unknown <esraamortada809@gmail.com>
2023-12-04 22:32:06 +0200

    first commit

---
+++ Products.js
@@ -0,0 +1,68 @@
+products=[{
+    id:1,
+    name:"Iphone",
+    description:`Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum vitae ullam, 
+    dolorem tempore cum rem laborum facilis dolores earum perferendis doloribus enim, optio ipsam ipsum?
+     Porro dolores eum sit autem. `,
+    image:"1.jpg",
+    stock:5,
+    price:555
+    
+},
+{
+    id:2,
+    name:"Iphone",
+    description:`Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum vitae ullam, 
+    dolorem tempore cum rem laborum facilis dolores earum perferendis doloribus enim, optio ipsam ipsum?
+     Porro dolores eum sit autem. `,
+    image:"1.jpg",
+    stock:10,
+    price:555
+    
+},
+{
+    id:3,
+    name:"Iphone",
+    description:`Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum vitae ullam, 
+    dolorem tempore cum rem laborum facilis dolores earum perferendis doloribus enim, optio ipsam ipsum?
+     Porro dolores eum sit autem. `,
+    image:"1.jpg",
+    stock:5,
+    price:555
+  
+
+},
+{
+    id:4,
+    name:"Iphone",
+    description:`Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum vitae ullam, 
+    dolorem tempore cum rem laborum facilis dolores earum perferendis doloribus enim, optio ipsam ipsum?
+     Porro dolores eum sit autem. `,
+    image:"1.jpg",
+    stock:0,
+    price:555
+    
+},
+{
+    id:5,
+    name:"Iphone",
+    description:`Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum vitae ullam, 
+    dolorem tempore cum rem laborum facilis dolores earum perferendis doloribus enim, optio ipsam ipsum?
+     Porro dolores eum sit autem. `,
+    image:"1.jpg",
+    stock:5,
+    price:555
+   
+},
+{
+    id:6,
+    name:"Iphone",
+    description:`Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum vitae ullam, 
+    dolorem tempore cum rem laborum facilis dolores earum perferendis doloribus enim, optio ipsam ipsum?
+     Porro dolores eum sit autem. `,
+    image:"1.jpg",
+    stock:15,
+    price:555
+    
+},
+]
---
+++ vue.js
@@ -0,0 +1,35 @@
+// const app =Vue.createApp({
+
+//     data:()=>({
+//         message:"hello from vue",
+//         degree: 90,
+//         para:"<h2> Esraa Mortada</h2>",
+//         students:[
+//             {name:"esraa", age:23 ,position:"front-end developer"},
+//             {name:"esraa", age:23 ,position:"front-end developer"},
+//             {name:"esraa", age:23 ,position:"front-end developer"},
+//             {name:"esraa", age:23 ,position:"front-end developer"}
+            
+//         ],
+//         counter:10
+
+//     }),
+//     methods:{
+//         showAlert:function(){
+//             alert('hello esraa hope you are fine');
+//         },
+//         finishexam:function(){
+//             alert("exam finished good luck");
+//         },
+//         startexam:function(){
+//             alert("exam started you have 50 minutes");
+//         }
+//     }
+
+//     })
+
+//     app.mount("#demo")
+
+
+
+
